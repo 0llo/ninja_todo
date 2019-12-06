773778fda29d262c14f4ee1ac63227738d043519
conan <conan@ecpmail.com>
2019-12-06 15:10:05 -0800

    done todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 //import "./style.css";
 
 class App extends Component {
@@ -9,14 +10,21 @@ class App extends Component {
       { id: 2, content: "play mario kart" }
     ]
   };
+  addTodo = todo => {
+    todo.id = Math.random();
+    const todos = [...this.state.todos, todo];
+    this.setState({ todos });
+  };
   deleteTodo = id => {
-    console.log(id);
+    const todos = this.state.todos.filter(todo => todo.id !== id);
+    this.setState({ todos });
   };
   render() {
     return (
       <div className="App container">
         <h1 className="center blue-text">Todo</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
